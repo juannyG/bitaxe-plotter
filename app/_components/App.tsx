cc58eaf3f88011c25e8b5231e042ca3dc7a28de2
Juan Gutierrez <juanny.gee@gmail.com>
2024-10-11 08:31:57 -0400

    Functional prototype for config drawer!

--- app/_components/App.tsx
+++ app/_components/App.tsx
@@ -17,28 +17,70 @@ const App = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <div
-        className="flex flex-col items-center h-full w-full"
+        className="flex flex-col items-center h-dvh w-full"
         data-theme={theme}
       >
-        <div className="flex justify-center pt-10">
-          <h1 className="text-2xl">Bitaxe Stat Plotter</h1>
-        </div>
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
 
-        <main className="flex flex-col items-center h-full w-full">
-          {children}
-        </main>
-        <footer className="flex flex-column p-10">
-          <label className="label pr-3">Set your theme:</label>
-          <select
-            className="select select-bordered"
-            value={theme}
-            onChange={(e) => selectNewTheme(e.target.value)}
-          >
-            <option value="light">Light</option>
-            <option value="dark">Dark</option>
-            <option value="dracula">Dracula</option>
-          </select>
-        </footer>
+          {/* Page content here */}
+          <label htmlFor="my-drawer" className="drawer-button">
+            <div className="visible h-dvh fixed border-r-4 border-slate-600 p-5 drawer-button cursor-pointer">
+              {'>>'}
+            </div>
+          </label>
+
+          <div className="flex justify-center pt-10">
+            <h1 className="text-2xl">Bitaxe Stat Plotter</h1>
+          </div>
+
+          <main className="flex flex-col items-center h-full w-full">
+            {children}
+          </main>
+
+          <footer className="flex flex-column justify-center p-10">
+            <label className="label pr-3">Set your theme:</label>
+            <select
+              className="select select-bordered"
+              value={theme}
+              onChange={(e) => selectNewTheme(e.target.value)}
+            >
+              <option value="light">Light</option>
+              <option value="dark">Dark</option>
+              <option value="dracula">Dracula</option>
+            </select>
+          </footer>
+
+        </div>
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+            {/* Sidebar content here */}
+            <li>
+              <a>
+                <div className="collapse">
+                  <input type="checkbox"  />
+                  <div className="collapse-title text-xl font-medium">
+                    Sidebar Item 1
+                  </div>
+                  <div className="collapse-content">
+                    <p>hello</p>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       </div>
     </>
   );
